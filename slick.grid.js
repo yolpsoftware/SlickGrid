c9f1e8734133de7298fa1a2c9cdf6e745972a5ae
Dave Benjamin <daveb@twitter.com>
2013-08-12 08:24:05 -0700

    Expose scrollPageUp and scrollPageDown methods

--- slick.grid.js
+++ slick.grid.js
@@ -3346,6 +3346,8 @@ if (typeof Slick === "undefined") {
       "navigateDown": navigateDown,
       "navigateLeft": navigateLeft,
       "navigateRight": navigateRight,
+      "scrollPageUp": scrollPageUp,
+      "scrollPageDown": scrollPageDown,
       "gotoCell": gotoCell,
       "getTopPanel": getTopPanel,
       "setTopPanelVisibility": setTopPanelVisibility,
